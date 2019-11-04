0f1b66a80c42df7a9c71be5e0a3dfd12c968faf9
Sam Precious <samwdp@gmail.com>
2019-11-04 09:49:04 +0000

    starting configuration

---
+++ config.el
@@ -0,0 +1,15 @@
+;;; .doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+;;
+
+(setq
+ user-full-name "Sam Precious"
+ display-line-numbers-type 'relative
+ )
+
+(setq
+ doom-font (font-spec :family "Fira Code" :size 14)
+ )
+
+(load-theme 'doom-gruvbox t)
