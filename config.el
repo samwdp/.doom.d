907462f94cf7d0293b91c6ff431b86c228ac500e
Sam Precious <sam.precious@nimbusdti.co.uk>
2020-08-06 09:13:50 +0100

    Updated config to be literate

--- config.el
+++ config.el
@@ -1,21 +1,11 @@
-;;; .doom.d/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here
-
-(setq
-display-line-numbers-type 'relative
-omnisharp-expected-server-version "1.35.4"
- )
-
-;; Theming
-(setq
- doom-font (font-spec :family "FiraCode NF" :size 14)
- doom-localleader-key "m"
- ;; projectile-project-search-path '("~/projects/nimbus" "~/projects/personal")
-                                        ; +mu4e-backend 'offlineimap
- doom-theme 'doom-gruvbox
- doom-gruvbox-brighter-comments 't
- )
+(setq user-full-name "Sam Precious")
+(setq user-mail-address "samwdp@gmail.com")
+(setq display-line-numbers-type 'relative)
+(setq doom-font (font-spec :family "FiraCode NF" :size 14))
+(setq  doom-theme 'doom-gruvbox
+ doom-gruvbox-brighter-comments 't)
 (setq +doom-dashboard-banner-dir
       (concat doom-private-dir "banners/"))
 (setq doom-modeline-major-mode-color-icon t)
+(setq omnisharp-expected-server-version "1.35.4")
+(setq doom-localleader-key "m" )
