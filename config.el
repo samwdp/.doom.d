fea3b75f657aefb855560139dfde18e341320839
Sam Precious <sam.precious@nimbusdti.co.uk>
2020-08-25 15:43:21 +0100

    updated to emacs 28

--- config.el
+++ config.el
@@ -1,7 +1,7 @@
 (setq user-full-name "Sam Precious")
 (setq user-mail-address "samwdp@gmail.com")
 (setq display-line-numbers-type 'relative)
- (setq doom-font (font-spec :family "FiraCode NF" :size 14))
+(setq doom-font (font-spec :family "Fira Code" :size 14))
 (setq doom-theme 'my-doom-gruvbox
       doom-gruvbox-brighter-comments t)
 (setq +doom-dashboard-banner-dir
@@ -10,11 +10,11 @@
 (toggle-frame-maximized)
 (setq projectile-indexing-method 'native)
 (setq ispell-program-name "hunspell")
-;;(org-babel-do-load-languages
-;; 'org-babel-load-languages
-;; '((csharp . t)
-;;   (powershell . t)
-;;   ))
+(org-babel-do-load-languages
+'org-babel-load-languages
+'((csharp . t)
+(powershell . t)
+))
 (setq projectile-indexing-method 'native)
 (setq omnisharp-expected-server-version "1.35.4")
 (setq doom-localleader-key "m" )
