5825a2fa5cbd2aaadf9dd2058bb0d5f804e792af
Sam Precious <samwdp@gmail.com>
2021-03-12 09:20:14 +0000

    lkjasdlklaksjdlkasj:

--- init.el
+++ init.el
@@ -42,7 +42,7 @@
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;; tabs              ; a tab bar for Emacs
-       ;; treemacs          ; a project drawer, like neotree but cooler
+       treemacs          ; a project drawer, like neotree but cooler
        unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
@@ -73,9 +73,9 @@
 
        :term
        eshell            ; the elisp shell that works everywhere
-       ;; shell             ; simple shell REPL for Emacs
+       shell             ; simple shell REPL for Emacs
        ;; term              ; basic terminal emulator for Emacs
-       vterm             ; the best terminal emulation in Emacs
+       ;; vterm             ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
@@ -179,5 +179,5 @@
        ;; twitter           ; twitter client https://twitter.com/vnought
 
        :config
-       literate
+       ;; literate
        (default +bindings +smartparens))
